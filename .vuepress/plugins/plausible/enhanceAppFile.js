8f872282cd69fb9f76011dec7d6e363e2d002583
Leo <losoviz@gmail.com>
2020-04-18 19:33:15 +0800

    Removed commented code

--- .vuepress/plugins/plausible/enhanceAppFile.js
+++ .vuepress/plugins/plausible/enhanceAppFile.js
@@ -1,5 +1,5 @@
-export default ({ router }) => {
-    if (process.env.NODE_ENV === 'production' && /*PLAUSIBLE_DOMAIN && */ typeof window !== 'undefined') {
+export default () => {
+    if (process.env.NODE_ENV === 'production' && typeof window !== 'undefined') {
         (function(w, d, s, o, f, js, fjs) {            
             w[o] = w[o] || function () {
                 (w[o].q = w[o].q || []).push(arguments)
@@ -8,17 +8,11 @@ export default ({ router }) => {
             js.id = o;
             js.src = f;
             js.async = 1;
-            // js['data-domain'] = PLAUSIBLE_DOMAIN;
             fjs.parentNode.insertBefore(js, fjs);
         } (window, document, 'script', 'plausible', 'https://plausible.io/js/p.js'))
         
         window.plausible('page')
         window.plausible('trackPushState')
-      
-        // router.afterEach(function (to) {
-        //     window.plausible('page')
-        //     window.plausible('trackPushState')
-        // })
     }
 }
 
--- .vuepress/plugins/plausible/index.js
+++ .vuepress/plugins/plausible/index.js
@@ -1,12 +1,5 @@
 const { path } = require('@vuepress/shared-utils')
 
 module.exports = (options = {}, context) => ({
-  // define () {
-  //   const { siteConfig = {}} = context
-  //   const plausibleDomain = options.domain || siteConfig.plausibleDomain
-  //   const PLAUSIBLE_DOMAIN = plausibleDomain || false
-  //   return { PLAUSIBLE_DOMAIN }
-  // },
-
   enhanceAppFiles: path.resolve(__dirname, 'enhanceAppFile.js')
 })
